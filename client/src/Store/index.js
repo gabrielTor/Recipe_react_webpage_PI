e5adc7acb43bd48f01dfb306ec823ffe55823759
gabrielTor <gabriel_torres2012@yahoo.com>
2022-09-05 13:02:57 -0300

    fixed issue with other pc compatibilty

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { rootReducer } from "../Reducers";
 import thunk from 'redux-thunk'
 
-const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ),
-  )
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+// const store = createStore(
+//   rootReducer,
+//   compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   ),
+// )
 
 export default store;
