d52a7239e8c4c5643e6ec32a38ccbc8a98ae473d
gabrielTor <gabriel_torres2012@yahoo.com>
2022-11-01 12:52:31 -0300

    register looks good

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -5,12 +5,14 @@ import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { userRegister } from "../../Reducers/actions";
 import { useDispatch } from "react-redux";
+import { Link, useHistory } from 'react-router-dom'
 
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 export default function Register(){
 
+    const history = useHistory()
     const dispatch = useDispatch()
     const [error, setError] = useState({
         email: true,
@@ -36,6 +38,7 @@ export default function Register(){
 
     const handleSubmit = () => {
         if(!error.email && !error.password) dispatch(userRegister(input))
+        history.push('/login')
     }
     const handleChange = (event) => {
         setInput((currentInput) => ({
@@ -49,47 +52,55 @@ export default function Register(){
             <h1>Register</h1>
             <div id='reg-container'>
                 <form id='register-form' onSubmit={handleSubmit}>
-                    <>
+
+                    <div className="items">
                     <label>First Name</label>
                     <input type='text' name='firstName' value={input.firstName} onChange={(e)=>handleChange(e)} required/>
-                    </>
+                    </div>
 
-                    <>
+                    <div className="items">
                     <label>Last Name</label>
                     <input type='text' name='lastName' value={input.lastName} onChange={(e)=>handleChange(e)} required/>
-                    </>
+                    </div>
 
-                    <>
+                    <div className="items">
                     <label>
                         Email
-                        {!error.email ? <FontAwesomeIcon icon={faCheck}/> : null}
-                        {!error.email || !input.email ? null : <FontAwesomeIcon icon={faTimes}/>}
+                        <FontAwesomeIcon icon={faCheck} className={!error.email ? "valid" : "hide"} />
+                        <FontAwesomeIcon icon={faTimes} className={!error.email || !input.email ? "hide" : "invalid"} />
                     </label>
                     <input type='text' autoComplete="off" name='email' value={input.email} onChange={(e)=>handleChange(e)} required/>
                     {!error.email || !input.email ? null :
-                    <p>
+                    <p className="box">
                         <FontAwesomeIcon icon={faInfoCircle}/>
                         input a valid email address
                     </p>}
-                    </>
+                    </div>
 
-                    <>
+                    <div className="items">
                     <label>
                         Password
-                        {!error.password ? <FontAwesomeIcon icon={faCheck}/> : null}
-                        {!error.password || !input.password ? null : <FontAwesomeIcon icon={faTimes}/>}
+                        {!error.password ? <FontAwesomeIcon icon={faCheck} className='valid'/> : null}
+                        {!error.password || !input.password ? null : <FontAwesomeIcon icon={faTimes} className='invalid'/>}
                     </label>
                     <input type='password' name='password' value={input.password} onChange={(e)=>handleChange(e)} required/>
                     {(!error.password || !input.password) ? null :
-                    <p>
+                    <p className="box">
                         <FontAwesomeIcon icon={faInfoCircle} />
                         8 to 24 characters.<br />
                         Must include uppercase and lowercase letters, a number and a special character.
                         Allowed special characters:<br></br> ! @ # $ %
                     </p>}
-                    </>
-                    <br></br>
-                    <button type="submit">Sign up</button>
+                    </div>
+
+                    <button className='register-btn' disabled={error.email || error.password} type="submit">Sign up</button>
+
+                    <p>
+                        Already registered?<br />
+                        <span className="line">
+                            <Link to="/login">Sign In</Link>
+                        </span>
+                    </p>
                 </form>
             </div>
         </section>
