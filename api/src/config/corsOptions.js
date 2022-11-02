253cbfc33fcc7ee8c98f0e78666c40ed5f52587d
gabrielTor <gabriel_torres2012@yahoo.com>
2022-11-02 19:30:37 -0300

    headers and methods

--- api/src/config/corsOptions.js
+++ api/src/config/corsOptions.js
@@ -19,8 +19,10 @@ const corsOptions = {
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true)
         res.header('Access-Control-Allow-Origin', origin)
+        res.header('Access-Control-Allow-Credentials', true)
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
     }
     next();
 }
