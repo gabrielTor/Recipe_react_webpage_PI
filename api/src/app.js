7631c45501bb3c5ff51c78e2a1220b84f56ab19b
gabrielTor <gabriel_torres2012@yahoo.com>
2022-11-02 20:33:44 -0300

    testing cors

--- api/src/app.js
+++ api/src/app.js
@@ -8,26 +8,23 @@ require('./db.js');
 
 const server = express();
 const cors = require('cors');
-const { credentials, corsOptions } = require('./config/corsOptions.js');
+// const { credentials, corsOptions } = require('./config/corsOptions.js');
 
 server.name = 'API';
 
 // server.use(credentials)
-server.use(cors({
-  origin: 'https://recipe-react-webpage-pi.vercel.app',
-  credentials: true
-}))
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 
