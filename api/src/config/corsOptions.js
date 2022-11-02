1974881bb7934a1967bc9852a510e35a261fb5f5
gabrielTor <gabriel_torres2012@yahoo.com>
2022-11-02 19:23:03 -0300

    allow origins

--- api/src/config/corsOptions.js
+++ api/src/config/corsOptions.js
@@ -19,7 +19,8 @@ const corsOptions = {
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Credentials', true)
+        res.header('Access-Control-Allow-Origin', origin)
     }
     next();
 }
