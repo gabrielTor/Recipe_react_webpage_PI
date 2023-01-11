a4b4ec6a3b6fd34ea2bacdd27a1e3b8b06d4e032
gabrielTor <gabriel_torres2012@yahoo.com>
2023-01-11 11:42:20 -0300

    pagination resize on window

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,21 +1,34 @@
+import { useState, useEffect } from 'react'
 import styles from './pagination.module.css'
 
 function Pagination({ page, setPage, nextP, prevP, numLength }){
+
+    const [windowSize, setWindowSize] = useState(window.outerWidth)
     const pageN = []
     for (let i = 1; i <= numLength; i++) {
         pageN.push(i)
     }
 
+    useEffect(() => {
+        const handleWindowResize = () => {
+          setWindowSize(window.outerWidth)
+        };
+    
+        window.addEventListener('resize', handleWindowResize)
+    
+        return () => {
+          window.removeEventListener('resize', handleWindowResize)
+        }
+    })
+
     return(
         <ul className={styles.pNums}>
             <li><button onClick={()=>prevP()}>&#10094;</button></li>
-            {
-                pageN?.map(n => {
-                return (
+            {windowSize < 560 ? null : 
+                pageN?.map(n => (
                     <li className={styles.btn} key={n}>
                         <button className={page === n ? styles.onPage : null} onClick={()=>setPage(n)}>{n}</button>
-                    </li>)
-                })
+                    </li>))
             }
             <li><button onClick={()=>nextP()}>&#10095;</button></li>
         </ul>
