1ddd510486cd79f279b66f40a5d520be4fe29a66
gabrielTor <gabriel_torres2012@yahoo.com>
2023-04-11 00:28:47 -0300

    https server

--- api/index.js
+++ api/index.js
@@ -1,29 +1,21 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const fs = require('fs')
+const https = require('https')
+const server = require('./src/app.js')
+const { conn } = require('./src/db.js')
+
+const key = fs.readFileSync(__dirname + '/private.key')
+const cert = fs.readFileSync(__dirname + '/certificate.crt')
+
+const credentials = {
+  key,
+  cert
+}
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
-//change back to force
+    console.log('%s listening at 3001')
+  })
+})
+const httpsServer = https.createServer(credentials, server)
+httpsServer.listen(8443)
--- api/src/app.js
+++ api/src/app.js
@@ -3,9 +3,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const fs = require('fs')
-
-const file = fs.readFileSync(__dirname + '/8185F175CE522CBF9AD4FC2662825BE6.txt')
 
 require('./db.js');
 
@@ -22,9 +19,9 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use('/', routes);
-server.get('/.well-known/pki-validation/8185F175CE522CBF9AD4FC2662825BE6.txt', (req, res) => {
-  res.sendFile(__dirname + '/8185F175CE522CBF9AD4FC2662825BE6.txt')
-})
+// server.get('/.well-known/pki-validation/8185F175CE522CBF9AD4FC2662825BE6.txt', (req, res) => {
+//   res.sendFile(__dirname + '/8185F175CE522CBF9AD4FC2662825BE6.txt')
+// })
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
