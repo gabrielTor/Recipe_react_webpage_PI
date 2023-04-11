8e1537df86c3607edf09192f2aa78adc1a3d4d79
gabrielTor <gabriel_torres2012@yahoo.com>
2023-04-11 00:10:53 -0300

    working text

--- api/src/app.js
+++ api/src/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const fs = require('fs')
 
-// const file = fs.readFileSync('./8185F175CE522CBF9AD4FC2662825BE6.txt')
+const file = fs.readFileSync(__dirname + '/8185F175CE522CBF9AD4FC2662825BE6.txt')
 
 require('./db.js');
 
@@ -21,17 +21,9 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
-
 server.use('/', routes);
 server.get('/.well-known/pki-validation/8185F175CE522CBF9AD4FC2662825BE6.txt', (req, res) => {
-  res.send('api\src\8185F175CE522CBF9AD4FC2662825BE6.txt')
+  res.sendFile(__dirname + '/8185F175CE522CBF9AD4FC2662825BE6.txt')
 })
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
